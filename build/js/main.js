ddce81740d3f7c01c3d21e04e09126db346e95bc
Chris Rau <98765332+rau03@users.noreply.github.com>
2024-12-16 19:35:15 -0600

    finsihed responsive menu and animation

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,15 @@
 const initApp = () => {
-    const hamburgerBtn = document.getElementById('hamburger-button')
-    const mobileMenu = document.getElementById('mobile-menu')
+  const hamburgerBtn = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
 
-    const toggleMenu = () => {
-        mobileMenu.classList.toggle('hidden')
-        mobileMenu.classList.toggle('flex')
-    }
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+    hamburgerBtn.classList.toggle("toggle-btn");
+  };
 
-    hamburgerBtn.addEventListener('click', toggleMenu)
-    mobileMenu.addEventListener('click', toggleMenu)
-}
+  hamburgerBtn.addEventListener("click", toggleMenu);
+  mobileMenu.addEventListener("click", toggleMenu);
+};
 
-document.addEventListener('DOMContentLoaded', initApp)
+document.addEventListener("DOMContentLoaded", initApp);
